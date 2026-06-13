e7c00de2deda8f2e717be8d1dad3338195d1177c
Arham <snehazahan@yahoo.com>
2026-06-12 20:43:40 -0400

    Add initial CyberPackMod class with Forge annotation

---
+++ src/main/main/java/com/cyber/cyberpack/CyberPackMod.java
@@ -0,0 +1,8 @@
+package com.cyber.cyberpack;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "cyberpack", name = "Cyber Starter Pack", version = "1.0.0")
+public class CyberPackMod {
+    // Forge handles registration automatically via annotations!
+}
