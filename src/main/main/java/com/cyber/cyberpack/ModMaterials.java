7505e53d7dfc2e5c0235fb12ab4ec900005fc838
Arham <snehazahan@yahoo.com>
2026-06-12 20:42:01 -0400

    Add ModMaterials class with CYBER tool material

---
+++ src/main/main/java/com/cyber/cyberpack/ModMaterials.java
@@ -0,0 +1,9 @@
+package com.cyber.cyberpack;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ModMaterials {
+    // Harvest Level 4, 3000 Uses, 12.0 Speed, 8.0 Base Damage, 22 Enchantability
+    public static final ToolMaterial CYBER = EnumHelper.addToolMaterial("CYBER", 4, 3000, 12.0F, 8.0F, 22);
+}
